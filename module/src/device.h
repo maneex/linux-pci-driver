baf2282e0ec560a66ee714a613d1aa9be7bdf02d
Perceval <perceval@trilogic.fr>
2026-08-15 20:18:49 +0200

    Initialize BAR0, read Magic.

---
+++ module/src/device.h
@@ -0,0 +1,20 @@
+#ifndef VELOCITOR_DEVICE_H
+#define VELOCITOR_DEVICE_H
+
+struct Device {
+  void __iomem *bar0;
+  void __iomem *bar2;
+  void __iomem *bar4;
+};
+
+enum InitialisationState {
+  INIT_STATE_PROBED,
+  INIT_STATE_ENABLED,
+  INIT_STATE_IOMAP,
+  INIT_STATE_COMPLETE
+};
+
+#define BAR0_MAGIC_VALUE 0x4F4C4556
+#define BAR0_MAGIC_OFFSET 0
+
+#endif // not VELOCITOR_DEVICE_H
--- module/src/lifecycle.c
+++ module/src/lifecycle.c
@@ -4,6 +4,7 @@
 #include <linux/pci.h>
 
 #include "../include/pci.h"
+#include "device.h"
 
 static struct pci_device_id pci_id_table[] = {
     {PCI_DEVICE(VELOCITOR_PCI_VENDORID, VELOCITOR_PCI_DEVICEID)},
@@ -13,19 +14,87 @@ static struct pci_device_id pci_id_table[] = {
 
 // https : // www.kernel.org/doc/html/v6.0/PCI/pci.html
 
+static int velocitor_release(struct pci_dev *dev,
+                             enum InitialisationState state, int ret) {
+  struct Device *device = pci_get_drvdata(dev);
+
+  // Disable the device from generating IRQs
+  if (INIT_STATE_PROBED != state)
+    pci_disable_device(dev);
+
+  // Release the IRQ (free_irq())
+  // Stop all DMA activity
+  // Release DMA buffers (both streaming and coherent)
+  // Unregister from other subsystems (e.g. scsi or netdev)
+
+  switch (state) {
+  case INIT_STATE_COMPLETE:
+    fallthrough;
+
+  case INIT_STATE_IOMAP: // Release MMIO/IOP resources
+    if (NULL != device->bar0)
+      pci_iounmap(dev, device->bar2);
+    if (NULL != device->bar2)
+      pci_iounmap(dev, device->bar2);
+    if (NULL != device->bar4)
+      pci_iounmap(dev, device->bar4);
+    pci_release_regions(dev);
+    fallthrough;
+
+  case INIT_STATE_ENABLED:
+    break;
+  case INIT_STATE_PROBED:
+    break;
+  }
+
+  return ret;
+}
+
+static int initialize_bar0(struct pci_dev *dev, struct Device *device) {
+  // Get mapping
+  device->bar0 = pci_iomap(dev, 0, 0);
+  if (NULL == device->bar0)
+    return -ENOMEM;
+
+  // Check MAGIC
+  u32 magic = readl(device->bar0 + BAR0_MAGIC_OFFSET);
+  if (BAR0_MAGIC_VALUE != magic) {
+    dev_err(&dev->dev,
+            "velocitor.bar0: invalid magic: got 0x%08x, expected 0x%08x\n",
+            magic, BAR0_MAGIC_VALUE);
+    return -ENODEV;
+  }
+  pr_info("velocitor.bar0: magic verified");
+
+  return 0;
+}
+
 static int velocitor_pci_probe(struct pci_dev *dev,
                                const struct pci_device_id *device_id) {
+
+  int err = 0;
+  struct Device *device =
+      devm_kzalloc(&(dev->dev), sizeof(struct Device), GFP_KERNEL);
+  if (NULL == device)
+    return -ENOMEM;
+  pci_set_drvdata(dev, device);
 
   pr_info("velocitor.pci_probe[%s]: device found", pci_name(dev));
 
   //  Enable the device
-  int err = pcim_enable_device(dev);
-  if (err)
-    return err;
-
+  if ((err = pcim_enable_device(dev)))
+    return velocitor_release(dev, INIT_STATE_PROBED, err);
   pr_info("velocitor.pci_probe[%s]: device enabled", pci_name(dev));
 
   // Request MMIO/IOP resources
+  if ((err = pci_request_regions(dev, KBUILD_MODNAME)))
+    return velocitor_release(dev, INIT_STATE_ENABLED, err);
+  pr_info("velocitor.pci_probe[%s]: request regions as %s", pci_name(dev),
+          KBUILD_MODNAME);
+
+  if ((err = initialize_bar0(dev, device)))
+    return velocitor_release(dev, INIT_STATE_IOMAP, err);
+
   // Set the DMA mask size (for both coherent and streaming DMA)
   // Allocate and initialize shared control data (pci_allocate_coherent())
   // Access device configuration space (if needed)
@@ -37,13 +106,7 @@ static int velocitor_pci_probe(struct pci_dev *dev,
 }
 
 static void velocitor_pci_remove(struct pci_dev *dev) {
-  // Disable the device from generating IRQs
-  // Release the IRQ (free_irq())
-  // Stop all DMA activity
-  // Release DMA buffers (both streaming and coherent)
-  // Unregister from other subsystems (e.g. scsi or netdev)
-  // Release MMIO/IOP resources
-  // Disable the device : automatically done by pcim
+  velocitor_release(dev, INIT_STATE_COMPLETE, 0);
 }
 
 static struct pci_driver velocitor_pci_driver = {
